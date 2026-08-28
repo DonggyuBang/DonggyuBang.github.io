dc1efd5f9919c0218d4e5f869daf4181aded9c6d
DonggyuBang <ahr1emd@kyonggi.ac.kr>
2026-08-28 16:34:28 +0900

    remove old BERL wbsite files

--- assets/images/README.txt
+++
@@ -1,1 +0,0 @@
-Place profile photos, lab photos, equipment photos, and logos in this folder. Then reference them from the JSON data files, e.g. assets/images/member-name.jpg.
--- assets/js/data-pages.js
+++
@@ -1,4 +0,0 @@
-async function renderCards(path, target, renderer){
-  const data=await BERL.json(path);
-  document.getElementById(target).innerHTML=data.map(renderer).join("");
-}
